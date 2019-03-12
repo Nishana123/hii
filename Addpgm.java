c39e75a1da667b69ea5da57ee02231d616d44e23
Nishana123 <nishanasalim79@gmail.com>
2019-03-12 11:00:09 +0530

    addition

---
+++ Addpgm.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+public class Addpgm{
+   public static void main(String[] args) {
+Scanner myinput=new Scanner(System.in);
+    int a,b,c;
+    System.out.println("Enter 1st number");
+    a=myinput.nextInt();
+    System.out.println("Enter 2nd number");
+    b=myinput.nextInt();
+    c=a+b;
+    System.out.println(c);
+
+      
+  }  
+}
